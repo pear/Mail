8c2cef6f4ec11e2f88c71ca45f1173fbd2fd11f6
Jon Parise <jon@php.net>
2003-08-10 19:29:10 +0000

    Wrapping long lines at 80 columns.
    git-svn-id: http://svn.php.net/repository/pear/packages/Mail/trunk@137272 c90b9560-bf6c-de11-be94-00142212c4b1

--- Mail/smtp.php
+++ Mail/smtp.php
@@ -120,8 +120,14 @@ class Mail_smtp extends Mail {
     {
         include_once 'Net/SMTP.php';
 
-        if (!($smtp = new Net_SMTP($this->host, $this->port))) { return new PEAR_Error('unable to instantiate Net_SMTP object'); }
-        if (PEAR::isError($smtp->connect())) { return new PEAR_Error('unable to connect to smtp server ' . $this->host . ':' . $this->port); }
+        if (!($smtp = new Net_SMTP($this->host, $this->port))) {
+            return new PEAR_Error('unable to instantiate Net_SMTP object');
+        }
+
+        if (PEAR::isError($smtp->connect())) {
+            return new PEAR_Error('unable to connect to smtp server ' .
+                                  $this->host . ':' . $this->port);
+        }
 
         if ($this->auth) {
             $method = is_string($this->auth) ? $this->auth : '';
@@ -147,14 +153,21 @@ class Mail_smtp extends Mail {
             return new PEAR_Error('No from address given');
         }
 
-        if (PEAR::isError($smtp->mailFrom($from))) { return new PEAR_Error('unable to set sender to [' . $from . ']'); }
+        if (PEAR::isError($smtp->mailFrom($from))) {
+            return new PEAR_Error('unable to set sender to [' . $from . ']');
+        }
 
         $recipients = $this->parseRecipients($recipients);
         foreach($recipients as $recipient) {
-            if (PEAR::isError($res = $smtp->rcptTo($recipient))) { return new PEAR_Error('unable to add recipient [' . $recipient . ']: ' . $res->getMessage()); }
+            if (PEAR::isError($res = $smtp->rcptTo($recipient))) {
+                return new PEAR_Error('unable to add recipient [' .
+                                      $recipient . ']: ' . $res->getMessage());
+            }
         }
 
-        if (PEAR::isError($smtp->data($text_headers . "\r\n" . $body))) { return new PEAR_Error('unable to send data'); }
+        if (PEAR::isError($smtp->data($text_headers . "\r\n" . $body))) {
+            return new PEAR_Error('unable to send data');
+        }
 
         $smtp->disconnect();
         return true;
