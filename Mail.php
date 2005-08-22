854011308e884dd97800429ed60113e3b887fc9c
Jon Parise <jon@php.net>
2005-08-22 01:19:27 +0000

    Mail::factory() now returns object references without generating PHP warnings.
    git-svn-id: http://svn.php.net/repository/pear/packages/Mail/trunk@194141 c90b9560-bf6c-de11-be94-00142212c4b1

--- Mail.php
+++ Mail.php
@@ -46,13 +46,14 @@ class Mail
      * @return object Mail a instance of the driver class or if fails a PEAR Error
      * @access public
      */
-    function factory($driver, $params = array())
+    function &factory($driver, $params = array())
     {
         $driver = strtolower($driver);
         @include_once 'Mail/' . $driver . '.php';
         $class = 'Mail_' . $driver;
         if (class_exists($class)) {
-            return new $class($params);
+            $mailer = new $class($params);
+            return $mailer;
         } else {
             return PEAR::raiseError('Unable to find class for driver ' . $driver);
         }
