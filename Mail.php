d5d101a91d923d4d53745390b87c36b881606e35
Aleksander Machniak <alec@php.net>
2011-01-14 19:06:57 +0000

    - Fixed Bug #17866: Non-static method Mail_RFC822::parseAddressList called statically
    git-svn-id: http://svn.php.net/repository/pear/packages/Mail/trunk@307489 c90b9560-bf6c-de11-be94-00142212c4b1

--- Mail.php
+++ Mail.php
@@ -250,7 +250,8 @@ class Mail
         // Parse recipients, leaving out all personal info. This is
         // for smtp recipients, etc. All relevant personal information
         // should already be in the headers.
-        $addresses = Mail_RFC822::parseAddressList($recipients, 'localhost', false);
+        $Mail_RFC822 = new Mail_RFC822();
+        $addresses = $Mail_RFC822->parseAddressList($recipients, 'localhost', false);
 
         // If parseAddressList() returned a PEAR_Error object, just return it.
         if (is_a($addresses, 'PEAR_Error')) {
